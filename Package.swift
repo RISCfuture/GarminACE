b171a2672bd5b14ef27349a29e4218c051c1942d
Tim Morgan <github@timothymorgan.info>
2023-01-19 18:39:42 -0800

    Lock down version requirements and version test coverage

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.6
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "GarminACE",
     defaultLocalization: "en",
-    platforms: [.iOS(.v11), .macOS(.v10_13), .tvOS(.v11), .watchOS(.v4)],
+    platforms: [.macOS(.v13), .iOS(.v16), .watchOS(.v9), .tvOS(.v16)],
     products: [
         .library(
             name: "GarminACE",
